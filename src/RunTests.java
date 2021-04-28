fffd0e3d059ce88339f8e1054a039ef512d7a410
Samhith G B <gbsamhith@gmail.com>
2021-04-28 10:25:24 -0400

    Fix

--- src/RunTests.java
+++ src/RunTests.java
@@ -11,15 +11,13 @@ public class RunTests {
         String [] testFileList = {"TestCharacterCount", "TestWordCount", "TestVowelCount"};
         for(String i : testFileList) {
             System.out.println("Working Directory = " + System.getProperty("user.dir"));
-            System.out.println("-------------------------------STARTED  " + i +"  -----------------------------------");
-            String[] command = new String[]{"java" , "-cp", System.getProperty("user.dir"),i, configFile};
+            String[] command = new String[]{"java" , "-cp", System.getProperty("user.dir") + File.separator + "out" + File.separator + "production" + File.separator+ "project_folder" ,"TestCharacterCount", configFile};
             ProcessBuilder pb = new ProcessBuilder(command).inheritIO();
             Process p = pb.start();
             p.waitFor();
             if(p.exitValue()!=0){
                 throw new AssertionError("Error while verificaiton");
             }
-            System.out.println("-------------------------------DONE-----------------------------------");
         }
 
         System.out.println("Done with running all the tests. Exiting..");
